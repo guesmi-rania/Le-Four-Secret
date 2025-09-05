1219ac2ba09408b1537b5327abff7ebf4b4d426b
rania.guesmi <rania.guesmi@esen.tn>
2025-09-05 15:58:47 +0000

    hhhhhelllll

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-// ✅ Import du CSS global
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
