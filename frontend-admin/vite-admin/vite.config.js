01a96f40490afc3d6133fe7fb370605b2098ee38
rania.guesmi <rania.guesmi@esen.tn>
2025-12-23 10:57:00 +0000

    new config

--- frontend-admin/vite-admin/vite.config.js
+++ frontend-admin/vite-admin/vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: path.resolve(__dirname, '../backend/public/admin'),
-    emptyOutDir: true,
-  },
-  server: {
-    port: 5174,
-  },
+  base: './', // essentiel pour que le build statique fonctionne
 });
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: path.resolve(__dirname, '../backend/public/client'),
-    emptyOutDir: true,
-  },
-  server: {
-    port: 5173,
-  },
+  base: './', // très important pour que le build fonctionne sur Render
 });
